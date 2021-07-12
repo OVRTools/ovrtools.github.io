e0a2401e8547c9c5518604bec40417696e09bc1e
TJ Horner <me@tjhorner.com>
2021-07-12 00:32:27 -0400

    Initial commit

---
+++ src/components/Project.svelte
@@ -0,0 +1,60 @@
+<script context="module" lang="ts">
+  export interface ProjectData {
+    name: string
+    description: string
+    link: string
+    tags: string[]
+  }
+</script>
+
+<script lang="ts">
+  export let project: ProjectData
+</script>
+
+<div class="project">
+  <div class="name">
+    <a href="{project.link}" target="_blank">{project.name}</a>
+  </div>
+  <div class="description">
+    {project.description}
+  </div>
+  <div class="tags">
+    {#each project.tags as tag}
+      <span class="tag">{tag}</span>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .project {
+    display: flex;
+    flex-direction: column;
+    color: white;
+    background: #2184bf;
+    padding: 10px;
+    border-radius: 5px;
+  }
+
+  a {
+    color: white;
+  }
+
+  .project > div:not(:last-of-type) {
+    margin-bottom: 10px;
+  }
+  
+  .name {
+    font-weight: bold;
+  }
+  
+  .description {
+    flex-grow: 1;
+  }
+
+  .tag {
+    display: inline-block;
+    padding: 5px;
+    background: #206690;
+    margin-right: 5px;
+  }
+</style>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import Project from "./Project.svelte"
+  import type { ProjectData } from "./Project.svelte"
+
+  const projects: ProjectData[] = [
+    {
+      name: "OVRSharp",
+      description: "High-level idiomatic C# interface for working with the OpenVR API.",
+      link: "https://github.com/OVRTools/OVRSharp",
+      tags: [
+        "library",
+        "c#"
+      ]
+    },
+    {
+      name: "WhereIsForward",
+      description: "Helps you find the front and center of your VR space with an arrow.",
+      link: "https://github.com/OVRTools/WhereIsForward",
+      tags: [
+        "app",
+        "c#"
+      ]
+    },
+    {
+      name: "OpenVRDeviceBattery",
+      description: "Taskbar app to monitor battery levels and see OpenVR device info.",
+      link: "https://github.com/OVRTools/OpenVRDeviceBattery",
+      tags: [
+        "app",
+        "c#"
+      ]
+    },
+    {
+      name: "ovrtools.github.io",
+      description: "This very website!",
+      link: "https://github.com/OVRTools/ovrtools.github.io",
+      tags: [
+        "svelte"
+      ]
+    }
+  ]
+</script>
+
+<div class="projects">
+  <h1>Projects</h1>
+
+  <div class="projects-inner">
+    {#each projects as project}
+      <Project {project}/>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .projects-inner {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    column-gap: 10px;
+		row-gap: 10px;
+  }
+</style>
